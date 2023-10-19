6918a72b1bd356ea38826c123fe7b8791d88c8e5
JP-Brito <jpbritoalm@gmail.com>
2023-10-18 21:14:24 -0300

    Adicionado interação com a kick, Adicinado styles para diferenciar os sites

--- script.js
+++ script.js
@@ -2,25 +2,42 @@ const lives = [
   ['jonvlogs', '00e7ebf9-89b9-41e1-b2bc-263b0ce805d0'],
   ['diegogelio_', '2211765a-e752-4812-9b53-84af4b9f9d31'],
   ['tiliaoficial', '236567d4-b236-47e6-aa2f-81e2d0af76c3'],
-  ['maumauzk_ofc', '829be78d-edcf-4c69-a30a-d6d73003744f'],
-  ['cabritoz', 'e536cf88-fc55-4438-941b-b228ab82166d'],
-  ['MeikodRJ', '9581207b-7255-4616-bfd0-4471a2e9de9e']
+  ['meikodrj', '9581207b-7255-4616-bfd0-4471a2e9de9e'],
+  ['maumauzk_ofc', '829be78d-edcf-4c69-a30a-d6d73003744f', 'maumauzk01'],
+  ['cabritoz', 'e536cf88-fc55-4438-941b-b228ab82166d', 'Chief117']
 ];
 const tamanholist = lives.length
 
 const livescontainer = document.getElementById("livesprofile");
-function createLives() {
-  lives.forEach(item => {
+async function createLives() {
+  lives.forEach((item) => {
 
-    var liveEmbed = new Twitch.Embed("twitch-embed", {
-      channel: item[0],
-      autoplay: false,
-      layout: 'video'
-    });
-    lives.push(liveEmbed)
+    if (item[2]) {
+      fetch(`https://kick.com/api/v2/channels/${item[2]}`)
+        .then(response => response.json())
+        .then(data => {
+          lives.push(data)
+        })
+    } else {
+
+      let liveEmbed = new Twitch.Embed("twitch-embed", {
+        channel: item[0],
+        autoplay: false,
+        layout: 'video'
+      });
+      lives.push(liveEmbed)
+    }
+
+
 
     const ancora = document.createElement('a')
-    ancora.href = `https://www.twitch.tv/${item[0]}`;
+    if (item[2]) {
+      ancora.href = `https://kick.com/${item[2]}`;
+      ancora.classList.add('kick')
+    } else {
+      ancora.href = `https://www.twitch.tv/${item[0]}`;
+      ancora.classList.add('twitch')
+    }
     ancora.target = '_blank'
     livescontainer.appendChild(ancora);
 
@@ -35,14 +52,24 @@ function atualizarLives() {
   location.reload();
 }
 function IsOnline() {
-  const imgs = document.querySelectorAll('img')
+  const imgs = document.querySelectorAll('.profile')
   imgs.forEach((item, index) => {
-    let idlive = lives[index + tamanholist]._player._playerState.duration
-    if (idlive === 0) {
-      item.style.borderColor = '#ff0000'
+    let calc = index + tamanholist
+    if (lives[index][2]) {
+      if (lives[calc]["livestream"]) {
+        item.style.borderColor = '#00ff00'
+      } else {
+        item.style.borderColor = '#ff0000'
+      }
+
     } else {
-      item.style.borderColor = '#00ff00'
+      if (lives[calc]._player._playerState.duration === Infinity) {
+        item.style.borderColor = '#00ff00'
+      } else {
+        item.style.borderColor = '#ff0000'
+      }
     }
+
   })
 }
 
